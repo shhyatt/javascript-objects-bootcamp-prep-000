2e39b1baf16bacd19be6ce1a09d5b7eb28b4fb6e
Samantha Hyatt <shhyatt@gmail.com>
2018-09-01 05:40:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function updatePlaylist(playlist, artist, songTitle) {
   playlist[artist] = songTitle;
   return playlist;
 }
-function removeFromPlaylist(playlist, artist, songTitle) { 
+function removeFromPlaylist(playlist, artist) { 
   delete playlist.artist;
 }
 
