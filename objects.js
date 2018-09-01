fe7d6d00a55092f42484d9f7d5229187a0aef3d3
Samantha Hyatt <shhyatt@gmail.com>
2018-09-01 05:32:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {artist: 'songTitle'};
-function updatePlaylist(playlist, artist, song title) {
-  return Object.assign({}, playlist, {[artist]: value})
+function updatePlaylist(playlist, artist, songTitle) {
+  return Object.assign({}, playlist, {[artist]: value});
 }
